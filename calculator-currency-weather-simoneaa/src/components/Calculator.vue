7192d0eac0b763181f2443a34d79c206fa358ee5
unknown <avilarranz@gmail.com>
2026-07-13 13:46:42 +0200

    feat:base calculator structure

---
+++ calculator-currency-weather-simoneaa/src/components/Calculator.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+    <div>
+        <input type="text"></input>
+        <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>/</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>*</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+            <button>+</button>
+            <button>CE</button>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
