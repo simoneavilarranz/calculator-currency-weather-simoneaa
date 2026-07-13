e8ad4df45cbd05a2ac009514b485cb42268d1e49
unknown <avilarranz@gmail.com>
2026-07-13 14:05:12 +0200

    feat:added add number basic functionality

--- calculator-currency-weather-simoneaa/src/App.vue
+++ calculator-currency-weather-simoneaa/src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import Calculator from './components/Calculator.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Calculator></Calculator>
 </template>
 
 <style scoped></style>
--- calculator-currency-weather-simoneaa/src/components/Calculator.vue
+++ calculator-currency-weather-simoneaa/src/components/Calculator.vue
@@ -1,26 +1,45 @@
-<script setup></script>
+<script setup>
+    import { ref } from 'vue';
+
+    const display = ref('0')
+
+    
+
+    function addNumber(num) {
+        if (display.value === '0') {
+            display.value = num
+        } else {
+            display.value += num
+        }
+    }
+
+    function clear() {
+        display.value = '0'
+    }
+
+</script>
 
 <template>
     <div>
-        <input type="text"></input>
+        <input type="text" v-model="display"></input>
         <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
+            <button @click="addNumber('7')">7</button>
+            <button @click="addNumber('8')">8</button>
+            <button @click="addNumber('9')">9</button>
             <button>/</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button @click="addNumber('4')">4</button>
+            <button @click="addNumber('5')">5</button>
+            <button @click="addNumber('6')">6</button>
             <button>*</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button @click="addNumber('1')">1</button>
+            <button @click="addNumber('2')">2</button>
+            <button @click="addNumber('3')">3</button>
             <button>+</button>
-            <button>0</button>
+            <button @click="addNumber('0')">0</button>
             <button>.</button>
             <button>=</button>
-            <button>+</button>
-            <button>CE</button>
+            <button>-</button>
+            <button @click="clear">CE</button>
         </div>
     </div>
 </template>
