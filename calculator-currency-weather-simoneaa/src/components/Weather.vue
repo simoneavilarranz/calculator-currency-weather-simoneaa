10e82122c0906ba875ac1ea2cd13aff57c214af2
unknown <avilarranz@gmail.com>
2026-07-15 14:09:40 +0200

    feat:started weather script setup

--- calculator-currency-weather-simoneaa/src/components/Weather.vue
+++ calculator-currency-weather-simoneaa/src/components/Weather.vue
@@ -1,13 +1,39 @@
 <script setup>
-     
+     import axios from 'axios';
+import { ref } from 'vue'
+
+    const viewMode = ref('nacional')
+    const weatherData = ref(null)
+
+    const skyImages = {
+        
+    }
+
+    async function weatherSpain() {
+        try{
+            const response = await axios.get('https://api.el-tiempo.net/json/v3/general')
+        }
+        catch (error) {
+            console.error('Error', error)
+        }
+    }
+
+    async function weatherAsturias() {
+        try{
+            const response = await axios.get('https://api.el-tiempo.net/json/v3/provincias/33')
+        }
+        catch (error) {
+            console.error('Error', error)
+        }
+    }
+
 </script>
 
 <template>
     <div class="bg-gray-700 rounded-xl shadow-2xl p-4 w-sm">
-        <input type="text" class="p-3 rounded-lg text-black font-regular text-lg w-full mb-4 bg-gray-100" v-model="current"></input>
         <div class="grid grid-cols-2 gap-2">
-            <button class="p-3 rounded-lg text-white font-bold text-md bg-red-500 hover:brightness-160">Spain</button>
-            <button class="p-3 rounded-lg text-white font-bold text-md bg-green-500 hover:brightness-130">Asturias</button>
+            <button class="p-3 rounded-lg text-white font-bold text-md bg-red-500 hover:brightness-160" @click="weatherSpain">Spain</button>
+            <button class="p-3 rounded-lg text-white font-bold text-md bg-green-500 hover:brightness-130" @click="weatherAsturias">Asturias</button>
             <h1 class="p-3 text-white font-bold text-lg text-center col-span-full">{{ currentWeather }}</h1>
             <div class="grid grid-cols-2 gap-2">
                 <h1 class="p-3 text-white font-bold text-lg text-center">Max</h1>
