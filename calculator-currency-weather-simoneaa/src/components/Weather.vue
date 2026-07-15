db83a62a264c04fa6ca2c4a6ae0877250ec67faf
unknown <avilarranz@gmail.com>
2026-07-15 13:51:43 +0200

    feat:base weather component structure

--- calculator-currency-weather-simoneaa/src/components/Weather.vue
+++ calculator-currency-weather-simoneaa/src/components/Weather.vue
@@ -4,16 +4,16 @@
 
 <template>
     <div class="bg-gray-700 rounded-xl shadow-2xl p-4 w-sm">
-        <input type="text" class="p-3 rounded-lg text-black font-regular text-lg w-full mb-4 bg-gray-100" v-model="amount"></input>
+        <input type="text" class="p-3 rounded-lg text-black font-regular text-lg w-full mb-4 bg-gray-100" v-model="current"></input>
         <div class="grid grid-cols-2 gap-2">
             <button class="p-3 rounded-lg text-white font-bold text-md bg-red-500 hover:brightness-160">Spain</button>
             <button class="p-3 rounded-lg text-white font-bold text-md bg-green-500 hover:brightness-130">Asturias</button>
             <h1 class="p-3 text-white font-bold text-lg text-center col-span-full">{{ currentWeather }}</h1>
             <div class="grid grid-cols-2 gap-2">
-                <h1>Max</h1>
-                <h1>Min</h1>
-                <h1>{{ maxTemp }}</h1>
-                <h1>{{ minTemp }}</h1>
+                <h1 class="p-3 text-white font-bold text-lg text-center">Max</h1>
+                <h1 class="p-3 text-white font-bold text-lg text-center">Min</h1>
+                <h1 class="p-3 text-white font-bold text-lg text-center">{{ maxTemp }}</h1>
+                <h1 class="p-3 text-white font-bold text-lg text-center">{{ minTemp }}</h1>
             </div>
             <img src="" alt="">
             <p>{{ weatherDesc }}</p>
