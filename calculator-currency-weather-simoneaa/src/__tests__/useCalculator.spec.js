6caa37fe6d3a69ab7da57f9c857787076f5e095f
unknown <avilarranz@gmail.com>
2026-07-20 12:32:44 +0200

    feat:added more succesful calculator tests

--- calculator-currency-weather-simoneaa/src/__tests__/useCalculator.spec.js
+++ calculator-currency-weather-simoneaa/src/__tests__/useCalculator.spec.js
@@ -12,4 +12,47 @@ describe('useCalculator', () => {
 
     expect(display.value).toBe('5')
   })
+
+  it('substract two numbers', () => {
+    const { display, addNumber, setOperator, calculate } = useCalculator()
+
+    addNumber('3')
+    setOperator('-')
+    addNumber('2')
+    calculate()
+
+    expect(display.value).toBe('1')
+  })
+
+  it('should do three operations in a row', () => {
+    const { display, addNumber, setOperator, calculate } = useCalculator()
+
+    addNumber('3')
+    setOperator('-')
+    addNumber('2')
+    setOperator('+')
+    addNumber('2')
+    calculate()
+
+    expect(display.value).toBe('3')
+  })
+
+  it('should show Error when dividing by zero', () => {
+    const { display, addNumber, setOperator, calculate } = useCalculator()
+
+    addNumber('5')
+    setOperator('/')
+    addNumber('0')
+    calculate()
+
+    expect(display.value).toBe('Error')
+  })
+  it('should clear when pressing CE', () => {
+    const { display, addNumber, clear } = useCalculator()
+
+    addNumber('5')
+    clear()
+
+    expect(display.value).toBe('0')
+  })
 })
